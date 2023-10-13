15e87e2f49d49d64a2dce6a1e7ac5be77588fc00
VeruHorena <136056794+VeruHorena@users.noreply.github.com>
2023-10-13 09:35:37 +0200

    neco

---
+++ kod_hodina_13_10/kod_hodina_13_10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Zadej citatel:");
+        int citatel = int.Parse(Console.ReadLine());
+        Console.WriteLine("Zadej jmenovatel:");
+        int jmenovatel = int.Parse(Console.ReadLine());
+        if (citatel < jmenovatel)
+        {
+            Console.WriteLine("Nelze zapisovat smisene cislo.");
+        }
+        else
+        {
+            if (jmenovatel == 0)
+                Console.WriteLine("neplatny zlomek");
+
+            if (citatel % jmenovatel == 0)
+            {
+                Console.WriteLine("Vysledne cislo je: " + citatel / jmenovatel);
+            }
+            else
+            {
+                int cele = citatel / jmenovatel;
+                int smisene = citatel - (cele * jmenovatel);
+                Console.WriteLine("Vysledek je: " + cele + " " + smisene + "/" + jmenovatel);
+
+            }
+
+        }
+    }
+}
