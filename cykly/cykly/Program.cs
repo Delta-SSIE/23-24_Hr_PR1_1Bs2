74930253b059da7452ff3a7f3b8294bcd779ae5e
VeruHorena <136056794+VeruHorena@users.noreply.github.com>
2023-10-30 10:05:20 +0100

    kod 30_10

---
+++ cykly/cykly/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Zadej cislo pro delitelnost:");
+        int delitel = int.Parse(Console.ReadLine());
+        int cislo = delitel + 1;
+
+        while (cislo % delitel != 0)
+        {
+            Console.WriteLine("Zadej cislo delitelne " + delitel + ":");
+            cislo = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Vysledek deleni cislem " + delitel + " je: " + cislo / delitel);
+
+        /* int cislo;
+          do{
+            Console.WriteLine("Zadej cislo delitelne 5: ");
+           cislo = int.Parse(Console.ReadLine());    
+          }while(cislo%5 != 0);
+          Console.WriteLine("Vysledek deleni cislem 5 je: " + cislo/5);*/
+    }
+}
