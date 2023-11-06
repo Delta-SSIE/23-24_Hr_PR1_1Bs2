ede330c7c5d759d76b9cfd8c7be867c33da64156
VeruHorena <136056794+VeruHorena@users.noreply.github.com>
2023-11-06 09:18:35 +0100

    minimum

---
+++ testovani_minima/testovani_minima/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int pozice = 0;
+        int minimum = 0;
+        int cislo = 1; //nastavim na hodnotu, ktera projde do cyklu
+
+        while (cislo > 0)
+        {
+            Console.WriteLine("Zadej hodnotu: "); //protoze nastavena hodnota nesmi byt zapocitana do zadanych cisel, hned nacteme od uzivatele spravne cislo
+            cislo = int.Parse(Console.ReadLine());
+            if (cislo > 0)
+            { //kdyby bylo cislo od uzivatele zaporne, nechci ho testovat, ani zapocitat jeho zadani do pozice
+                pozice++;
+                if (pozice == 1)
+                { //pokud je zadavano prvni cislo, automaticky se stava maximem (pokud bych to neudelala, zadna hodnota by nebyla mensi nez 0, protoze by to automaticky ukoncovalo cyklus)
+                    minimum = cislo;
+                }
+                else
+                  if (pozice % 4 == 0)
+                { //v zadani je, ze mame testovat pouze pozice na nasobcich 4
+                    if (cislo < minimum)
+                    { //otestuju hodnotu
+                        minimum = cislo;
+                    }
+                }
+
+
+            }
+        }
+        if (pozice < 4)
+        {
+            Console.WriteLine("Zadano mene cisel nez 4.");
+        }
+        else
+        {
+            Console.WriteLine("Pocet zadanych cisel je: " + pozice);
+            Console.WriteLine("Minimum je: " + minimum);
+        }
+    }
+}
