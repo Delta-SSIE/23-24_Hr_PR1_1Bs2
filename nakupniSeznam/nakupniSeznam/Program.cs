8e36fe2932229cf8b0d1ac90985d19de38c7b48d
VeruHorena <136056794+VeruHorena@users.noreply.github.com>
2024-03-08 10:13:35 +0100

    menu

---
+++ nakupniSeznam/nakupniSeznam/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Vitejte v nakupnim seznamu 1.0");
+int volba;
+while (true)
+{
+   volba = menu();
+    if (volba == 0)
+    {
+        Console.WriteLine("Dekujeme za vyuziti programu.");
+        break;
+    }
+    else {
+        switch (volba) {
+            case 1: break;
+            case 2: break;
+            case 3: break;
+            case 4: break;
+        }
+    }
+}
+static int menu() {
+    int volba;
+    while (true)
+    {
+        Console.WriteLine("1. Vytvor seznam");
+        Console.WriteLine("2. Pridej polozky");
+        Console.WriteLine("3. Vyhledej");
+        Console.WriteLine("4. Odeber polozku");
+        Console.WriteLine("0 Ukoncit program");
+        if (int.TryParse(Console.ReadLine(), out volba) && (volba>=0 && volba<=4) )
+            break;
+        else
+        { 
+            Console.WriteLine("Neplatna volba.");
+            continue;
+        }
+
+    }
+        return volba;
+}
